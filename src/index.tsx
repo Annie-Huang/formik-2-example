1d7a7d355f369c512f9937e76f2c10985293a851
Annie Huang <annie.hn@gmail.com>
2021-01-26 18:57:52 +1100

    Remove StrictMode as material-ui select does not support it.

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+/*
+Remove StrictMode as material-ui select does not support it.
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+*/
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
