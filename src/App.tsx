b8298a9b2b6563ecc7f4e26c5e163fc6d35bfa41
Annie Huang <annie.hn@gmail.com>
2021-01-26 13:46:39 +1100

    Use Formik and materuial-ui to create a form with one text input.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { Formik } from "formik";
+import { TextField } from "@material-ui/core";
+
 function App() {
   return (
     <div>
+      <Formik
+        initialValues={{ firstName: "bob" }}
+        onSubmit={(data) => {
+          console.log(data);
+        }}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <TextField
+              name="firstName"
+              value={values.firstName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
